e19a0f8041985fed2aac42195c8f6f29fcb319cb
Magdalena.V <magdalena.vurmovska@hotmail.com>
2025-12-03 12:26:22 +0100

    Add new landing page layout and styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 // src/App.js
 import React from "react";
+import LandingPage from "./components/LandingPage.jsx";
 import AuthPage from "./components/AuthPage.jsx";
 import MeetupList from "./components/MeetupsPage.jsx";
 import "./index.css";
 
 function App() {
+  return <LandingPage />;
   return <AuthPage />;
   return <MeetupList />;
 
